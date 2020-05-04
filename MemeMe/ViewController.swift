b1d2868c7e8161ed6b6840271c0093c79fd2261d
Oscar Santos <oscarsantos.go@gmail.com>
2020-05-03 20:35:49 -0400

    add: imageView and toolbar on storyboard

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pickAnImage(_ sender: Any) {
+        
+    }
+    
 }
 
