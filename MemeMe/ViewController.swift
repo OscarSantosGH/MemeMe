358b98794ab566e3d9d7947751e4be10cfb4cade
Oscar Santos <oscarsantos.go@gmail.com>
2020-05-05 23:41:33 -0400

    add: texts fields

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
-    
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
