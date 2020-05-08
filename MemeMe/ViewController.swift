ea37704ce611923abc094b8c366dbd7f83b86350
Oscar Santos <oscarsantos.go@gmail.com>
2020-05-07 22:06:55 -0400

    add: meme textAttributes for the textFields

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -20,10 +20,24 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewDidLoad()
         topTextField.delegate = self
         bottomTextField.delegate = self
+        setMemeTextAttributes()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+    private func setMemeTextAttributes(){
+        let memeTextAttributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedString.Key.strokeWidth:  -3
+        ]
+        topTextField.defaultTextAttributes = memeTextAttributes
+        topTextField.textAlignment = .center
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.textAlignment = .center
     }
 
     @IBAction func pickAnImageFromCamera(_ sender: Any) {
