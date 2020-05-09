84b74e05c838093b676bf2419b25d671d733848a
Oscar Santos <oscarsantos.go@gmail.com>
2020-05-09 12:08:14 -0400

    add: Meme model

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,16 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Oscar Santos on 5/9/20.
+//  Copyright © 2020 Oscar Santos. All rights reserved.
+//
+
+import UIKit
+
+struct Meme {
+    let topText: String
+    let bottomText: String
+    let originalImage: UIImage
+    let memedImage: UIImage
+}
