22590740f66da60aff8c24f0145f1f260699e4a6
Oscar Santos <oscarsantos.go@gmail.com>
2020-05-10 23:18:31 -0400

    add: Impact font

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -41,7 +41,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let memeTextAttributes: [NSAttributedString.Key: Any] = [
             NSAttributedString.Key.strokeColor: UIColor.black,
             NSAttributedString.Key.foregroundColor: UIColor.white,
-            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedString.Key.font: UIFont(name: "impact", size: 40)!,
             NSAttributedString.Key.strokeWidth:  -3
         ]
         topTextField.defaultTextAttributes = memeTextAttributes
