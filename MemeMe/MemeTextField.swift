a23a1461f8d277d23dbfbe33a6d8bb411efd683b
Oscar Santos <oscarsantos.go@gmail.com>
2020-05-12 00:19:47 -0400

    add: selected font style button has a different appearance

--- MemeMe/MemeTextField.swift
+++ MemeMe/MemeTextField.swift
@@ -23,7 +23,6 @@ class MemeTextField: UITextField {
     }
     
     private func configure(){
-        setMemeTextAttributes(withFontName: "impact")
         availableFontsArray = [impactFontBtn, helveticaFontBtn, robotoFontBtn]
         containerStackView.frame = CGRect(x: 0, y: 0, width: frame.width, height: 40)
         containerStackView.alignment = .center
@@ -39,28 +38,41 @@ class MemeTextField: UITextField {
         helveticaFontBtn.addTarget(self, action: #selector(setHelveticaFont), for: .touchUpInside)
         
         robotoFontBtn.setTitle("ROBOTO", for: .normal)
-        robotoFontBtn.addTarget(self, action: #selector(setPacificoFont), for: .touchUpInside)
+        robotoFontBtn.addTarget(self, action: #selector(setRobotoFont), for: .touchUpInside)
+        
+        configureButtonsAppearances()
         
         for fontBtn in availableFontsArray{
-            fontBtn.setTitleColor(.black, for: .normal)
-            fontBtn.showsTouchWhenHighlighted = true
-            fontBtn.backgroundColor = .white
-            fontBtn.layer.cornerRadius = 5
-
             containerStackView.addArrangedSubview(fontBtn)
         }
+        
+        setImpactFont()
     }
     
     @objc func setImpactFont(){
         setMemeTextAttributes(withFontName: "impact")
+        configureButtonsAppearances()
+        impactFontBtn.backgroundColor = UIColor.init(white: 1, alpha: 0.8)
     }
     
     @objc func setHelveticaFont(){
         setMemeTextAttributes(withFontName: "HelveticaNeue-CondensedBlack")
+        configureButtonsAppearances()
+        helveticaFontBtn.backgroundColor = UIColor.init(white: 1, alpha: 0.8)
     }
     
-    @objc func setPacificoFont(){
+    @objc func setRobotoFont(){
         setMemeTextAttributes(withFontName: "Roboto-Bold")
+        configureButtonsAppearances()
+        robotoFontBtn.backgroundColor = UIColor.init(white: 1, alpha: 0.8)
+    }
+    
+    private func configureButtonsAppearances(){
+        for fontBtn in availableFontsArray{
+            fontBtn.setTitleColor(.black, for: .normal)
+            fontBtn.backgroundColor = UIColor.init(white: 0.5, alpha: 0.8)
+            fontBtn.layer.cornerRadius = 5
+        }
     }
     
     private func setMemeTextAttributes(withFontName name:String){
